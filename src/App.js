2a35a2500975688e2c343269a445a21c36fe4203
Mouhamed120 <65665285+Mouhamed120@users.noreply.github.com>
2022-04-22 16:41:30 +0200

    22/04/2022 Dans cette partie nous avons appris les à récupérer les données d'un formulaire

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [formData, setFormData] = useState({ nom: "", prenom: "", email: "" });
+  const handleChange = (e) => {
+    const obj = { ...formData, [e.target.name]: e.target.value };
+    setFormData(obj);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    setFormData({ nom: "", prenom: "", email: "" });
+  };
   return (
-    <div className="App">
-      <p>Hello React</p>
+    <div>
+      <h1>Inscription</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Nom</label>
+          <input
+            type="text"
+            name="nom"
+            onChange={handleChange}
+            value={formData.nom}
+          />
+        </div>
+        <br />
+        <div>
+          <label>Prenom</label>
+          <input
+            type="text"
+            name="prenom"
+            onChange={handleChange}
+            value={formData.prenom}
+          />
+        </div>
+        <div>
+          <br />
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            value={formData.email}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
